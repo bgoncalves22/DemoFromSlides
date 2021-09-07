89927da3d04b96b284ca9c65be32ceb866dc1708
goncalvesbrandon <bgoncalves@student.bridgew.edu>
2021-09-07 10:47:05 -0400

    End of Tuesdays Class

---
+++ src/Example1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args){
+        var studentNames = new ArrayList<String>();
+        var reader = new Scanner(System.in);
+        var userResponse = "";
+        while (!userResponse.equals("done")){
+            System.out.print("Enter ext ame or 'done' to end:");
+        }
+
+    }
+}
